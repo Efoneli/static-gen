79d2e03dd6b5e6a3135e5a99f1c1bdb96aa2178b
efoneli <abbaefoneli@gmail.com>
2023-04-16 21:56:20 +0100

    test

--- practice/src/pages/users.js
+++ practice/src/pages/users.js
@@ -1,7 +1,15 @@
-function UserDetails() {
+function UserDetails({ users }) {
     return (
         <>
         <h1>this is users page</h1>
+        {users.map((user) => {
+            return (
+                <div key={user.id}>
+                    <p> {user.name} </p>
+                    <p> {user.email } </p>
+                </div>
+            )
+        })}
         </>
     )
 }
@@ -12,4 +20,10 @@ export async function getStaticProps() {
     const response = await fetch('https://jsonplaceholder.typicode.com/users')
     const data = await response.json()
     console.log(data)
+
+    return {
+        props: {
+            users: data,
+        }
+    }
 }

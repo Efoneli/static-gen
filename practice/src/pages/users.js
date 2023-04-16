d72a5a7555ff9c0f16afc5cc2c9f08f6e4d92569
efoneli <abbaefoneli@gmail.com>
2023-04-16 21:44:35 +0100

    test

--- practice/src/pages/users.js
+++ practice/src/pages/users.js
@@ -7,3 +7,9 @@ function UserDetails() {
 }
 
 export default UserDetails
+
+export async function getStaticProps() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await response.json()
+    console.log(data)
+}
